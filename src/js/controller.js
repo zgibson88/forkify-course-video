6015e451a607cc1da3a493a6c334bd446ed63091
zgibson88 <zgibson88@yahoo.com>
2021-06-07 12:51:31 -0700

    test

--- src/js/controller.js
+++ src/js/controller.js
@@ -146,6 +146,10 @@ async function controlAddRecipe(newRecipe) {
     };
 };
 
+function newFeature() {
+    console.log("test");
+}
+
 function init() {
     bookmarksView.addHandlerRender(controlBookmarks);
     recipeView.addHandlerRender(controlRecipes);
@@ -154,6 +158,7 @@ function init() {
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
     addRecipeView.addHandlerUpload(controlAddRecipe);
+    newFeature();
 };
 
 init();
